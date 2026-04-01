117a4d80eef58e8af3df236a37884fe6d0f89299
SashaCarton <sashacarton2005@gmail.com>
2026-04-01 11:17:25 +0200

    Add symfony/mailer and symfony/monolog-bundle dependencies
    Fix: Class "Monolog\Logger" not found error on production

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
