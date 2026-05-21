905896365424858ac0eed2de77b141f8eb69903c
SashaCarton <sashacarton2005@gmail.com>
2026-05-21 13:32:13 +0200

    feat(search): ajouter le filtre texte par nom et ville
    Expose le paramètre `query` dans la whitelist du controller et ajoute
    un filtre LIKE sur `establishment_name` et `city` dans le repository.

--- src/Controller/LaundromatController.php
+++ src/Controller/LaundromatController.php
@@ -48,7 +48,7 @@ class LaundromatController extends AbstractApiController
 
         $filters = array_intersect_key(
             $request->query->all(),
-            array_flip(['address', 'services', 'paymentMethods', 'equipmentTypes']),
+            array_flip(['query', 'address', 'services', 'paymentMethods', 'equipmentTypes']),
         );
 
         $boundingBox = $this->snapBoundingBoxForCache(
